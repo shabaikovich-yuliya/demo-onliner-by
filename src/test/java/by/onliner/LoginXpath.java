967b71ebfdc63339cfe146609a5eb0fffd8d156d
Yuliya <yuliya.01707@gmail.com>
2024-10-31 18:08:52 +0100

    add LoginXpath class

---
+++ src/test/java/by/onliner/LoginXpath.java
@@ -0,0 +1,7 @@
+package by.onliner;
+
+public class LoginXpath {
+    public static final String INPUT_USERNAME_XPATH = "//*[@id=\"auth-container\"]/div/div[2]/div/form/div[1]/div/div[2]/div/div/div/div/input";
+    public static final String INPUT_PASSWORD_XPATH = "//*[@id=\"auth-container\"]/div/div[2]/div/form/div[2]/div/div/div/div/input";
+    public static final String BUTTON_SIGNIN_XPATH = "//*[@id=\"auth-container\"]/div/div[2]/div/form/div[3]/button";
+}
