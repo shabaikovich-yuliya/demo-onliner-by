2fdbf9f801a4e337a3fef76c12eaf4a04f53959f
Yuliya <yuliya.01707@gmail.com>
2024-10-31 19:41:01 +0100

    add additional xpathes

--- src/test/java/by/onliner/LoginXpath.java
+++ src/test/java/by/onliner/LoginXpath.java
@@ -4,4 +4,8 @@ public class LoginXpath {
     public static final String INPUT_USERNAME_XPATH = "//*[@id=\"auth-container\"]/div/div[2]/div/form/div[1]/div/div[2]/div/div/div/div/input";
     public static final String INPUT_PASSWORD_XPATH = "//*[@id=\"auth-container\"]/div/div[2]/div/form/div[2]/div/div/div/div/input";
     public static final String BUTTON_SIGNIN_XPATH = "//*[@id=\"auth-container\"]/div/div[2]/div/form/div[3]/button";
+    public static final String ERROR_MESSAGE_PASSWORD_IS_REQUIRED_XPATH = "//*[@id=\"auth-container\"]/div/div[2]/div/form/div[2]/div/div/div[2]/div";
+    public static final String ERROR_MESSAGE_USERNAME_IS_REQUIRED_XPATH = "//*[@id=\"auth-container\"]/div/div[2]/div/form/div[1]/div/div[2]/div/div/div[2]/div";
+    public static final String ERROR_MESSAGE_INVALID_USERNAME_OR_PASSWORD_XPATH = "//*[@id=\"auth-container\"]/div/div[2]/div/form/div[3]/div";
+    public static final String BUTTON_TO_FOLLOW_LOGIN_FORM_XPATH = "//*[@id=\"userbar\"]/div[2]/div/div/div[1]";
 }
